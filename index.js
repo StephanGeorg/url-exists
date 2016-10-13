5a941bab1038f4e47cc26a99b997adcec14464be
Stephan Georg <s.georg@nearest.place>
2016-10-13 20:36:34 +0200

    Added User-Agent

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var request = require('request');
 
 function urlExists(url, cb) {
-  request({ url: url, method: 'HEAD' }, function(err, res) {
-    if (err) return cb(null, false);
+  request({ 
+    url: url, 
+    method: 'HEAD',
+    headers: { 'User-Agent': 'url-exists' }
+  }, function(err, res) {
+    if (err) return cb(err, false);
     cb(null, /4\d\d/.test(res.statusCode) === false);
   });
 }
