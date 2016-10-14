52552642d46b2a72c5598132ea714474d679969e
Stephan Georg <s.georg@nearest.place>
2016-10-14 16:19:28 +0200

    Changed method to get and headers

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var request = require('request');
 
 function urlExists(url, cb) {
+  var headers = { "Accept": "text/html", "User-Agent": "Mozilla/5.0" };
   request({ 
     url: url, 
-    method: 'HEAD',
-    headers: { 'User-Agent': 'url-exists' }
+    method: 'GET',
+    headers: headers
   }, function(err, res) {
     if (err) return cb(err, false);
     cb(null, /4\d\d/.test(res.statusCode) === false);
